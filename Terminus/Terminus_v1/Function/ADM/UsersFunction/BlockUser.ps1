b835aef3d573ef3f4b6d4fc828f5fb7bcd34f9e7
LazyScriptTurtle <162825856+LazyScriptTurtle@users.noreply.github.com>
2024-03-10 10:07:02 +0100

    Blokada i usuwanie

---
+++ Terminus/Terminus_v1/Function/ADM/UsersFunction/BlockUser.ps1
@@ -0,0 +1,25 @@
+
+
+function BlockUser {
+try {
+    
+$imie = Read-Host "Podaj imię"
+$nazwisko = Read-Host "Podaj nazwisko"
+$email = Read-Host "Podaj adres email"
+
+$user = Get-ADUser -Filter "GivenName -eq '$imie' -and Surname -eq '$nazwisko' -and EmailAddress -eq '$email'" -Properties EmailAddress
+
+if ($null -ne $user)
+{
+    Disable-ADAccount -Identity "$user" -Confirm:$false
+}
+else {
+    Write-Host "Uztkownik nie istnieje"
+}
+
+}
+catch {
+    Write-Host "Blad: $_."
+}
+
+}
---
+++ Terminus/Terminus_v1/Function/ADM/UsersFunction/RemoveUser.ps1
@@ -0,0 +1,33 @@
+
+
+function RemoveUser {
+    try {
+
+# Zbieranie danych wejściowych od użytkownika
+$imie = Read-Host "Podaj imię"
+$nazwisko = Read-Host "Podaj nazwisko"
+$email = Read-Host "Podaj adres email"
+
+# Wyszukiwanie użytkownika w Active Directory
+$uzytkownik = Get-ADUser -Filter "GivenName -eq '$imie' -and Surname -eq '$nazwisko' -and EmailAddress -eq '$email'" -Properties EmailAddress
+
+if ($null -ne $uzytkownik) {
+    # Jeśli znaleziono użytkownika, wyświetl potwierdzenie
+    $odpowiedz = Read-Host "Czy na pewno chcesz usunąć użytkownika $($uzytkownik.Name) (t/n)?"
+    if ($odpowiedz -eq 't') {
+        # Usuwanie użytkownika
+        Remove-ADUser -Identity $uzytkownik -Confirm:$false
+        Write-Host "Użytkownik został usunięty."
+    } else {
+        Write-Host "Anulowano usuwanie użytkownika."
+    }
+} else {
+    # Jeśli użytkownik nie został znaleziony
+    Write-Host "Nie znaleziono użytkownika spełniającego podane kryteria."
+}
+
+    }
+    catch {
+        Write-Host "Błąd: $_"
+    }
+}
