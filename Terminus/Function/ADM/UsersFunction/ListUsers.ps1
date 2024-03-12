be9c7841c49edfc9cdcefaed1a4e3e78aa922cc0
LazyScriptTurtle <162825856+LazyScriptTurtle@users.noreply.github.com>
2024-03-12 18:19:03 +0100

    12.03.2024
    ListUsers

---
+++ Terminus/Function/ADM/UsersFunction/ListUsers.ps1
@@ -0,0 +1,22 @@
+
+
+function ListUsers {
+    try {
+
+$users = Get-ADUser -Filter * -Properties DistinguishedName, SamAccountName | Sort-Object Name
+
+$groupedUsers = $users | Group-Object { ($_ -split ',',2)[-1] }
+
+foreach ($group in $groupedUsers) {
+    Write-Output "`nOU=$($group.Name)"
+    foreach ($user in $group.Group) {
+        Write-Output $($user.SamAccountName)
+    }
+}
+
+}
+catch {
+  Read-Host "Blad: $_."
+}
+    
+}
